d7ecc94b0f6e297aefe28e2780c50b5facef9895
Felice Caricati <felice.caricati@openmindonline.it>
2018-10-30 10:35:57 +0100

    Fix multiple script injection close #1

--- content.js
+++ content.js
@@ -1,3 +1,17 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('shame.js');
-(document.head || document.documentElement).appendChild(s);
+var src = chrome.extension.getURL('shame.js');
+var script = document.querySelector('script[src="' + src + '"]');
+
+if (script) {
+  createScript(true);
+} else {
+  createScript(false);
+}
+
+function createScript(remove) {
+  if (remove) {
+    script.parentNode.removeChild(script);
+  }
+  var s = document.createElement('script');
+  s.src = src;
+  (document.head || document.documentElement).appendChild(s);
+}
